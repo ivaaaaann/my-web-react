d833dcbd764061b3a03b4f7ffdd31e086ed9a5cf
ldh3907 <ldh165163@gmail.com>
2021-06-09 19:50:30 +0900

    add : nav scrool event
    add : nav scrool event

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,60 @@
+import React, { Component } from "react";
 import "./Navigation.css";
 
-function Navigation() {
-  return (
-    <section id="navigation">
-      <div className="navigation-container">
-        <div className="nav-logo-img">
-          <img
-            src="https://image.flaticon.com/icons/png/128/639/639394.png"
-            alt="logo"
-            title="logo"
-          />
+class Navigation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      scrollTop: 0,
+      isScroll: false,
+    };
+  }
+  componentDidMount = () => {
+    window.addEventListener("scroll", this.handleScroll);
+  };
+
+  componentWillUnMount = () => {
+    window.removeEventListener("scroll", this.handleScroll);
+  };
+  handleScroll = (e) => {
+    const scrollTop = ("scroll", e.srcElement.scrollingElement.scrollTop);
+    console.log(scrollTop);
+    this.setState({
+      scrollTop,
+    });
+
+    if (scrollTop >= 432 && scrollTop < 1054) {
+      this.isScroll = true;
+      return;
+    }
+    this.isScroll = false;
+  };
+  render() {
+    return (
+      <section id="navigation">
+        <div
+          className={
+            this.isScroll
+              ? "navigation-container nav-after"
+              : "navigation-container"
+          }
+          onScroll={this.handleScroll}
+        >
+          <div className="nav-logo-img">
+            <img
+              src="https://image.flaticon.com/icons/png/128/639/639394.png"
+              alt="logo"
+              title="logo"
+            />
+          </div>
+          <div className="nav-item-container">
+            <div className="nav-item font-style">프로젝트</div>
+            <div className="nav-item font-style">소개</div>
+          </div>
         </div>
-        <div className="nav-item-container">
-          <div className="nav-item font-style">프로젝트</div>
-          <div className="nav-item font-style">소개</div>
-        </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 
 export default Navigation;
